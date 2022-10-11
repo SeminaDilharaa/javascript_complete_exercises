676922bb84a65c18e7f4f52956ed8eec810c93a5
Semina Dilhara <45710872+SeminiDilhara@users.noreply.github.com>
2022-10-11 17:25:55 +0530

    Add files via upload

---
+++ Javascript_Basics/Ex 04-Assignment Operators/script.js
@@ -0,0 +1,47 @@
+// Assignment Operators
+
+
+//Equal
+
+var myname = "Kasun";
+console.log(myname);
+
+//Addition with equal
+
+var age = 24;
+age +=10;//age= age+10
+console.log(age);
+
+
+//Substraction with equal
+
+var age = 24;
+age -=10;//age= age-10
+console.log(age);
+
+//Multiplication with equal
+
+var age = 24;
+age *=10;//age= age*10
+console.log(age);
+
+//Division with equal
+
+var age = 24;
+age /=4;//age= age/4
+console.log(age);
+
+//Modulus with equal
+
+var age = 24;
+age %=7;//age= age%7
+console.log(age);
+
+
+//Exponentiation with equal
+
+var age = 8;
+age **=3;//age= age*age
+console.log(age);
+
+
